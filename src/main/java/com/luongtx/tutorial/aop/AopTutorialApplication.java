a8b035c89419c354e0563c36f33a202a9135072a
luongtx <v.luongtx@vinhomes.vn>
2022-11-29 09:58:37 +0700

    spring aspectj

---
+++ src/main/java/com/luongtx/tutorial/aop/AopTutorialApplication.java
@@ -0,0 +1,18 @@
+package com.luongtx.tutorial.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class AopTutorialApplication {
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(AopTutorialApplication.class, args);
+        MessageCommunicator messageCommunicator = context.getBean(MessageCommunicator.class);
+        messageCommunicator.deliver("wanna learn aspjecj");
+        messageCommunicator.deliver("Harry", "having fun");
+    }
+}
---
+++ src/main/java/com/luongtx/tutorial/aop/SecurityAspect.java
@@ -0,0 +1,21 @@
+package com.luongtx.tutorial.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class SecurityAspect {
+    private final Authenticator authenticator = new Authenticator();
+
+    @Pointcut("execution(* com.luongtx.tutorial.aop.MessageCommunicator.*(..))")
+    public void secureAccess() {}
+
+    @Before("secureAccess()")
+    public void before() {
+       authenticator.authenticate();
+    }
+}
